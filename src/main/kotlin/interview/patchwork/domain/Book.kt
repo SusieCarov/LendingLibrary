d2f945bf6784544109089908bdd2af56a3099859
SusieC <susiecarovillanocode@gmail.com>
2024-02-01 21:19:32 +0000

    :construction: - Add UUID id to Book and a util extension to LibraryTest

--- src/main/kotlin/interview/patchwork/domain/Book.kt
+++ src/main/kotlin/interview/patchwork/domain/Book.kt
@@ -1,3 +1,11 @@
 package interview.patchwork.domain
 
-data class Book(val title: String, val author: String, val isbn: String, val status: String)
+import java.util.UUID
+
+data class Book(
+    val title: String,
+    val author: String,
+    val isbn: String,
+    val status: String,
+    val id: UUID = UUID.randomUUID()
+)
--- src/test/kotlin/interview/patchwork/LibraryTest.kt
+++ src/test/kotlin/interview/patchwork/LibraryTest.kt
@@ -4,28 +4,48 @@ import interview.patchwork.domain.Book
 import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.collections.shouldContainExactly
+import java.util.UUID
+
+fun Int.toUUID(): UUID =
+    UUID.fromString("00000000-0000-0000-a000-${this.toString().padStart(11, '0')}")
 
 class LibraryTest :
     FeatureSpec({
       val book1a =
-          Book(title = "Intro to Coding", author = "John Doe", isbn = "1234", status = "Available")
+          Book(
+              title = "Intro to Coding",
+              author = "John Doe",
+              isbn = "1234",
+              status = "Available",
+              id = 0.toUUID())
       val book1b =
-          Book(title = "Intro to Coding", author = "John Doe", isbn = "1234", status = "Available")
+          Book(
+              title = "Intro to Coding",
+              author = "John Doe",
+              isbn = "1234",
+              status = "Available",
+              id = 1.toUUID())
       val book2 =
           Book(
-              title = "Intro to Coding", author = "John Smith", isbn = "2345", status = "Available")
+              title = "Intro to Coding",
+              author = "John Smith",
+              isbn = "2345",
+              status = "Available",
+              id = 2.toUUID())
       val book3 =
           Book(
               title = "Anyone Can Cook",
               author = "Average Joe",
               isbn = "3456",
-              status = "Available")
+              status = "Available",
+              id = 3.toUUID())
       val book4 =
           Book(
               title = "Adventure is Out There!",
               author = "Average Joe",
               isbn = "4567",
-              status = "Available")
+              status = "Available",
+              id = 4.toUUID())
 
       val library = Library(mutableListOf(book1a, book1b, book2, book3, book4))
 
